cd8a4f5549ee225f6f51f7bb9f8cd762a250b378
dizmerly <dan.izmerly@gmail.com>
2024-02-19 16:43:07 -0600

    Added playGame function, fixed bugs

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const choice = ["R", "P", "S"];
+let pS;
+let cS;
+
 
 function getComputerChoice(){
     let n = Math.floor(Math.random() * 3);
@@ -12,25 +15,46 @@ function getPlayerChoice(playerChoice){
 
 
 function playRound(){
-    let pS = getPlayerChoice();
-    let cS = getComputerChoice();
+    pS = getPlayerChoice();
+    cS = getComputerChoice();
 
     if((pS == "R" && cS == "S") || (pS == "P" && cS == "R") || (pS == "S" && cS == "P")){
-        return "Computer chose " + cS + " \nPlayer Wins!";
+        return 1;
+    
     }
 
     else if((pS == "R" && cS == "P") || (pS == "S" && cS == "R") || (pS == "P" && cS == "S")){
-        return "Computer chose " + cS + " \nComputer Wins!";
+        return 0; 
     }
 
     else{
-        return "Computer chose " + cS + " \nGame Tied.";
+        return -1;
+        
     }
 }
 
 function playGame(){
+    let score = 0; 
     for(let i = 0; i <= 5; i++){
-        console.log(playRound());
+        let result = playRound()
+
+        if(result == 1){
+            console.log("Computer chose " + cS + " \nPlayer Wins Round!");
+            score++; 
+        }
+        else if(result == 0){
+            console.log("Computer chose " + cS + " \nComputer Wins Round!");
+        }
+        else{
+            console.log("Computer chose " + cS + " \nRound Tied.");
+        }
+    }
+
+    if(score > 2){
+        console.log("Player Wins Game!")
+    }
+    else{
+        console.log("Computer Wins Game!")
     }
 }
 
