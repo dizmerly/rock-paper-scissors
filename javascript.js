e13f69da5f25b42010d189c392f4ea12d07cecf0
dizmerly <dan.izmerly@gmail.com>
2024-02-22 10:09:42 -0600

    Removed console game logic

--- javascript.js
+++ javascript.js
@@ -33,29 +33,4 @@ function playRound(){
     }
 }
 
-function playGame(){
-    let score = 0; 
-    for(let i = 0; i < 5; i++){
-        let result = playRound()
 
-        if(result == 1){
-            console.log("Computer chose " + cS + " \nPlayer Wins Round!");
-            score++; 
-        }
-        else if(result == 0){
-            console.log("Computer chose " + cS + " \nComputer Wins Round!");
-        }
-        else{
-            console.log("Computer chose " + cS + " \nRound Tied.");
-        }
-    }
-
-    if(score > 2){
-        console.log("Player Wins Game!")
-    }
-    else{
-        console.log("Computer Wins Game!")
-    }
-}
-
-playGame();
